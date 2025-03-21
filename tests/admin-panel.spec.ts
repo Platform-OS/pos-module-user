231f907b84fecd7d33354b31afbc028e59f7b6ba
damcik <suchuschy@gmail.com>
2025-03-21 13:27:54 +0100

    add admin subscription test

--- tests/admin-panel.spec.ts
+++ tests/admin-panel.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test';
 import { AdminHomePage } from './pages/admin/home';
+import { SubscriptionPage } from './pages/subscription';
 import { HomePage } from './pages/home';
 import { users } from './data/users';
 
@@ -10,7 +11,19 @@ test.describe('Testing Admin Panel', () => {
     const adminHomePage = new AdminHomePage(page);
 
     await adminHomePage.goto();
-    await expect(adminHomePage.elementWithText('You have access to admin.read.')).toBeVisible();
+    await expect(adminHomePage.elementWithText('If you are seeing this page, you have been granted permissions admin.read.')).toBeVisible();
+  });
+
+  test('admin can access subscription area', async ({ browser }) => {
+    const context = await browser.newContext({ storageState: `tests/.auth/${users.superadmin.email}.json` });
+    const page = await context.newPage();
+    const homePage = new HomePage(page);
+    const subscriptionPage = new SubscriptionPage(page);
+
+    await homePage.goto();
+    await homePage.linkWithText('Subscription area').click();
+
+    await expect(subscriptionPage.elementWithText('If you are seeing this page, you have been granted permissions subscription.read.')).toBeVisible();
   });
 
   test('admin impersonate tests', async ({ browser }) => {
